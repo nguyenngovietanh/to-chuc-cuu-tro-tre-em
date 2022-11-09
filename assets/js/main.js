566cea7ff32e5dab5be3d1a0cfebee3425671024
anhnnv137 <nguyenngovietanh19@gmail.com>
2022-11-09 16:07:04 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+const product = [
+    {
+        id: 1,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 2,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 3,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 4,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 5,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 6,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 7,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    },
+    {
+        id: 8,
+        image: "/images/blog/ajax-articles/marios-restaurant-thumb.jpg",
+        title: "",
+        description: "",
+        itemCategory: "",
+        itemTime: "",
+        UserPageVisits: "",
+    }
+]
+
+let perPage = 4;
+let currentPage = 1;
+let start = 0;
+let end = perPage;
+
+function renderProduct() {
+    html = "";
+    const content = product.map((item, index) => {
+        
+    })
+}
